c01fb09baac09dc8edf9130592a2699cf3b44654
TrinhDuy94 <72688837+TrinhDuy94@users.noreply.github.com>
2021-07-21 23:11:15 +0700

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,1 +1,26 @@
-print("Hello world")
+import getpass
+import sys
+import telnetlib
+
+HOST = ""
+user = input("enter username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("Username: ")
+tn.write(user + "\n")
+if password:
+	tn.read_until("Password: ")
+	tn.write(password + "\n")
+
+tn.write("conf ter\n")
+
+for n in range (2,10):
+	tn.write("vlan "+str(n)+"\n")
+	tn.write("name Py_VLAN_"+str(n)+"\n")
+
+tn.write("end\n")
+tn.write("exit\n")
+
+print tn.read_all()
